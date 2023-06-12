9ea9173fb66c832bfe40c607bd429de0d48db233
mnikonov <mnikonov@live.ru>
2023-06-11 23:45:03 -0400

    Added github repository and contribution information to Settings page Resolves #7

---
+++ src/Gpt.Labs/Helpers/Extensions/PackageVersionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Windows.ApplicationModel;
+
+namespace Gpt.Labs.Helpers.Extensions
+{
+    public static class PackageVersionExtensions
+    {
+        public static string GetStringVersion(this PackageVersion version)
+        {
+            return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+    }
+}
